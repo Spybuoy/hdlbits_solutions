776a7a944a58f922079b0c65a1e7deb238effea5
chakri-seas <paladi.chakradhar01@gmail.com>
2024-10-26 12:36:12 -0700

    Comment updates

--- Random modules/barrel_shifter.v
+++ Random modules/barrel_shifter.v
@@ -8,9 +8,9 @@ module barrel_shifter();
 
 	always@(*)
 		begin
-			s0 = amt[0] ? {x[0], x[7:1]} : x; 
-			s1 = amt[1] ? {s0[1:0], s0[7:2]} : s0;
-			y  = amt[2] ? {s1[3:0], s1[7:4]} : s1;
+			s0 = amt[0] ? {x[0], x[7:1]} : x; 			// Shifting bit 0 or 1 bit
+			s1 = amt[1] ? {s0[1:0], s0[7:2]} : s0;		// Shifting bit 0 or 2 bits
+			y  = amt[2] ? {s1[3:0], s1[7:4]} : s1;		// Shifting bit 0 or 4 bits	
 		end
 
 
